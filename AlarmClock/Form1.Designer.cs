17970a3de0f3bb10e30938cb6760e0bf7062530e
Joel Gary A/L John Santhana Dass <Joel.Santhana@uct.com>
2024-11-25 15:18:40 +0800

    Alarm functionality added.

--- AlarmClock/Form1.Designer.cs
+++ AlarmClock/Form1.Designer.cs
@@ -29,41 +29,144 @@
         private void InitializeComponent()
         {
             components = new System.ComponentModel.Container();
-            timeLabel = new Label();
             timer1 = new System.Windows.Forms.Timer(components);
+            tabTime = new TabControl();
+            tabPage1 = new TabPage();
+            timeLabel = new Label();
+            tabPage2 = new TabPage();
+            btnStop = new Button();
+            AlarmLabel = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            buttonSetTime = new Button();
+            timer2 = new System.Windows.Forms.Timer(components);
+            tabTime.SuspendLayout();
+            tabPage1.SuspendLayout();
+            tabPage2.SuspendLayout();
             SuspendLayout();
-            // 
-            // timeLabel
-            // 
-            timeLabel.AutoSize = true;
-            timeLabel.Font = new Font("Segoe UI Emoji", 20F);
-            timeLabel.Location = new Point(334, 77);
-            timeLabel.Name = "timeLabel";
-            timeLabel.Size = new Size(109, 46);
-            timeLabel.TabIndex = 0;
-            timeLabel.Text = "label1";
-            timeLabel.Click += timeLabel_Click;
             // 
             // timer1
             // 
             timer1.Enabled = true;
             timer1.Tick += timer1_Tick;
             // 
+            // tabTime
+            // 
+            tabTime.Controls.Add(tabPage1);
+            tabTime.Controls.Add(tabPage2);
+            tabTime.Dock = DockStyle.Fill;
+            tabTime.Location = new Point(0, 0);
+            tabTime.Name = "tabTime";
+            tabTime.SelectedIndex = 0;
+            tabTime.Size = new Size(800, 450);
+            tabTime.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(timeLabel);
+            tabPage1.Location = new Point(4, 29);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(792, 417);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Clock";
+            tabPage1.UseVisualStyleBackColor = true;
+            tabPage1.Click += tabPage1_Click;
+            // 
+            // timeLabel
+            // 
+            timeLabel.AutoSize = true;
+            timeLabel.Font = new Font("Segoe UI Emoji", 20F);
+            timeLabel.Location = new Point(338, 140);
+            timeLabel.Name = "timeLabel";
+            timeLabel.Size = new Size(109, 46);
+            timeLabel.TabIndex = 0;
+            timeLabel.Text = "label1";
+            // 
+            // tabPage2
+            // 
+            tabPage2.Controls.Add(btnStop);
+            tabPage2.Controls.Add(AlarmLabel);
+            tabPage2.Controls.Add(dateTimePicker1);
+            tabPage2.Controls.Add(buttonSetTime);
+            tabPage2.Location = new Point(4, 29);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(792, 417);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "Alarm";
+            tabPage2.UseVisualStyleBackColor = true;
+            tabPage2.Click += tabPage2_Click;
+            // 
+            // btnStop
+            // 
+            btnStop.Location = new Point(510, 310);
+            btnStop.Name = "btnStop";
+            btnStop.Size = new Size(94, 29);
+            btnStop.TabIndex = 4;
+            btnStop.Text = "Stop Time";
+            btnStop.UseVisualStyleBackColor = true;
+            btnStop.Click += button1_Click;
+            // 
+            // AlarmLabel
+            // 
+            AlarmLabel.AutoSize = true;
+            AlarmLabel.Font = new Font("Segoe UI", 20F);
+            AlarmLabel.Location = new Point(328, 194);
+            AlarmLabel.Name = "AlarmLabel";
+            AlarmLabel.Size = new Size(111, 46);
+            AlarmLabel.TabIndex = 3;
+            AlarmLabel.Text = "Status";
+            AlarmLabel.Click += AlarmLabel_Click_1;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            dateTimePicker1.Format = DateTimePickerFormat.Time;
+            dateTimePicker1.Location = new Point(261, 97);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(243, 51);
+            dateTimePicker1.TabIndex = 2;
+            // 
+            // buttonSetTime
+            // 
+            buttonSetTime.Location = new Point(178, 310);
+            buttonSetTime.Name = "buttonSetTime";
+            buttonSetTime.Size = new Size(94, 29);
+            buttonSetTime.TabIndex = 1;
+            buttonSetTime.Text = "Set Time";
+            buttonSetTime.UseVisualStyleBackColor = true;
+            buttonSetTime.Click += buttonSetTime_Click;
+            // 
+            // timer2
+            // 
+            timer2.Tick += timer2_Tick;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(timeLabel);
+            Controls.Add(tabTime);
             Name = "Form1";
             Text = "Form1";
+            tabTime.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            tabPage2.ResumeLayout(false);
+            tabPage2.PerformLayout();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
-
-        private Label timeLabel;
         private System.Windows.Forms.Timer timer1;
+        private TabControl tabTime;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private Label timeLabel;
+        private System.Windows.Forms.Timer timer2;
+        private DateTimePicker dateTimePicker1;
+        private Button buttonSetTime;
+        private Label AlarmLabel;
+        private Button btnStop;
     }
 }
--- AlarmClock/Form1.cs
+++ AlarmClock/Form1.cs
@@ -2,22 +2,109 @@ namespace AlarmClock
 {
     public partial class Form1 : Form
     {
+        private DateTime alarmTime;
+        System.Timers.Timer timer;
+
         public Form1()
         {
             InitializeComponent();
             timer1.Tick += new EventHandler(timer1_Tick);
             timer1.Start();
+            //timer2 = new Timer();
+            timer2.Interval = 1000;
+            timer2.Tick += timer2_Tick;
+            timer = new System.Timers.Timer();
+            timer.Interval = 1000;
+            timer.Elapsed += timer_Elapsed;
         }
 
 
         private void timeLabel_Click(object sender, EventArgs e)
         {
-            timer1.Start();
+
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
             timeLabel.Text = DateTime.Now.ToString("HH:mm:ss");
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AlarmLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (DateTime.Now >= alarmTime)
+            {
+
+                timer2.Stop();
+                AlarmLabel.Text = "Alarm ringing";
+            }
+        }
+
+
+        private void buttonSetTime_Click(object sender, EventArgs e)
+        {
+            timer.Start();
+            AlarmLabel.Text = "Running....";
+
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        delegate void UpdateLabel(Label lbl, string value);
+        void UpdateDataLabel(Label lbl, string value)
+        {
+            lbl.Text = value;
+        }
+
+
+        private void timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            DateTime currTime = DateTime.Now;
+            DateTime userTime = dateTimePicker1.Value;
+            if (currTime.Hour == userTime.Hour && currTime.Minute == userTime.Minute && currTime.Second == userTime.Second)
+            {
+                timer.Stop();
+                try
+                {
+                    UpdateLabel upd = UpdateDataLabel;
+                    if (AlarmLabel.InvokeRequired)
+                        Invoke(upd, AlarmLabel, "Stop");
+                    // AlarmLabel.Text = "Alarm set for ringing";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+            AlarmLabel.Text = "Stopped.";
+        }
+
+        private void AlarmLabel_Click_1(object sender, EventArgs e)
+        {
+
         }
     }
 }
